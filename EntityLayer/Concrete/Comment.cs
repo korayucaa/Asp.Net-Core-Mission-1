6fdf8cc88cb254a6546c342eceaa3c47486a205a
korayucaa <vaynegg687@hotmail.com>
2024-05-16 17:22:15 +0300

    Mission-2

--- EntityLayer/Concrete/Comment.cs
+++ EntityLayer/Concrete/Comment.cs
@@ -14,7 +14,6 @@ namespace EntityLayer.Concrete
         public int CommentID { get; set; }
 		public string CommentUserName { get; set; }
 		public string CommentContent { get; set; }
-		public string CommentTitle { get; set; }
 		public DateTime CommentDate { get; set; }
         public bool CommentStatus { get; set; }
 		public int BlogID { get; set; }
